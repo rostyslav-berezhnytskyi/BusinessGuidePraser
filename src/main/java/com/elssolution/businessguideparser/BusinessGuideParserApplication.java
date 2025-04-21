84123f058062d44801699e61c458f54d3af11efd
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2025-04-21 09:54:32 +0300

    Initial commit: Project setup with Spring Boot Web, Batch, and Lombok

---
+++ src/main/java/com/elssolution/businessguideparser/BusinessGuideParserApplication.java
@@ -0,0 +1,13 @@
+package com.elssolution.businessguideparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessGuideParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessGuideParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elssolution/businessguideparser/BusinessGuideParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elssolution.businessguideparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessGuideParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
